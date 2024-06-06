5b4f410248e4638ccb85eafe63eea85cbe3aa39f
Juniorkone225 <115947328+Juniorkone225@users.noreply.github.com>
2024-06-06 16:54:32 +0000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -112,3 +112,25 @@ ScrollReveal().reveal(".pp", {
   distance: "60px",
   interval: 400,
 });
+$(document).ready(function () {
+  $(".image-popup-vertical-fit").magnificPopup({
+    type: "image",
+    mainClass: "mfp-with-zoom",
+    gallery: {
+      enabled: true,
+    },
+
+    zoom: {
+      enabled: true,
+
+      duration: 300, // duration of the effect, in milliseconds
+      easing: "ease-in-out", // CSS transition easing function
+
+      opener: function (openerElement) {
+        return openerElement.is("img")
+          ? openerElement
+          : openerElement.find("img");
+      },
+    },
+  });
+});
